01af7ba569cb4bfe74c253f9b20466c9d02eeac0
Panov Sergey <passt@yandex.ru>
2020-12-07 16:31:28 +0300

    init fix

--- Sources/PastySwift/PastySwift.swift
+++ Sources/PastySwift/PastySwift.swift
@@ -4,6 +4,10 @@ import UIKit
 open class PastRectangle {
     var rectangle: UIView?
 
+    public init() {
+
+    }
+
     open func createRectangle(frame: CGRect = .zero, color: UIColor) -> UIView {
         let rect = UIView(frame: frame)
         rect.backgroundColor = color
